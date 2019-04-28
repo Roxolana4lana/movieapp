1352cd2e2e9d6b2c8a4998a606c5b4b023c5d612
Roxolana <roxolanapavlyk@gmail.com>
2019-04-28 13:27:00 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Movie from "./components/Movie";
+import Store from "./components/Store";
+import "./styles/output.css";
+import Navigation from "./components/Navigation";
 
-class App extends Component {
-  render() {
-    return (
+function App() {
+  return (
+    <Store>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation />
+        <Movie />
       </div>
-    );
-  }
+    </Store>
+  );
 }
 
 export default App;
+//<Movie />
