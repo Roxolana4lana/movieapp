2f35a5e98bcfc20f71a569f844b2783d575cf897
Roxolana <roxolanapavlyk@gmail.com>
2019-04-28 21:58:26 +0200

    Updates

---
+++ precache-manifest.9022f84c724c9c5b2375bef70e8637e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  },
+  {
+    "revision": "4b55e62779cdf55910de",
+    "url": "/movieapp/static/js/main.933abedf.chunk.js"
+  },
+  {
+    "revision": "8f4ce9918f9c2b41193c",
+    "url": "/movieapp/static/js/2.359a48c9.chunk.js"
+  },
+  {
+    "revision": "4b55e62779cdf55910de",
+    "url": "/movieapp/static/css/main.7f1a6de5.chunk.css"
+  },
+  {
+    "revision": "109dfdcbc49025a8110e0b60d884e2ee",
+    "url": "/movieapp/index.html"
+  }
+];
